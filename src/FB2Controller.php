c2f6b8ceab4cd17aa94c0f536ef9b9f2e26a9dd6
Inkoder <web.inkoder@gmail.com>
2022-05-02 18:24:02 +0300

    Add PHPDoc comment for Book model var.

--- src/FB2Controller.php
+++ src/FB2Controller.php
@@ -8,73 +8,78 @@ use Tizis\FB2\Helpers\FileHandler;
 
 /**
  * Class FB2Controller
+ *
  * @package FB2
  */
 class FB2Controller extends FB2AttributesManager
 {
 
-  protected $xml;
-  protected $book;
+    protected $xml;
+    /**
+     * @var Model\Book
+     */
+    protected $book;
 
-  /**
-   * FB2Controller constructor.
-   * @param $file
-   */
-  public function __construct($file)
-  {
-    $this->xml = FileHandler::FB2FileCleaner($file);
-  }
-
-  /**
-   *  start parse book
-   */
-  public function startParse(): void
-  {
-    $FB2Parser = new FB2Parser($this->xml);
-    $FB2Parser->loadAttributes($this->getAttributes());
-    $FB2Parser->startParse();
-    $this->book = $FB2Parser->getBook();
-  }
-
-  public function getBook()
-  {
-    return $this->book;
-  }
-
-  public function getAuthors()
-  {
-    return $this->book->authors;
-  }
-
-  public function getTranslators()
-  {
-    return $this->book->translators;
-  }
-
-  /**
-   * @param array $attributes
-   */
-  public function withImages(array $attributes): void
-  {
-    $this->insertAttributes(true, 'isImages');
-    foreach ($attributes as $key => $attribute) {
-      if ($key === 'directory') {
-        $this->insertAttributes($attribute, 'imagesDirectory');
-      }
-      if ($key === 'imagesWebPath') {
-        $this->insertAttributes((string)$attribute, 'imagesWebPath');
-      }
+    /**
+     * FB2Controller constructor.
+     *
+     * @param $file
+     */
+    public function __construct($file)
+    {
+        $this->xml = FileHandler::FB2FileCleaner($file);
     }
-    $fileSystem = new Filesystem();
-    try {
-      $fileSystem->mkdir($this->getAttributes()['imagesDirectory']);
-    } catch (IOExceptionInterface $exception) {
-      echo 'An error occurred while creating your directory at' . $exception->getPath();
-    }
-  }
 
-  public function withNotes(): void
-  {
-    $this->insertAttributes(true, 'isNotes');
-  }
+    /**
+     *  start parse book
+     */
+    public function startParse(): void
+    {
+        $FB2Parser = new FB2Parser($this->xml);
+        $FB2Parser->loadAttributes($this->getAttributes());
+        $FB2Parser->startParse();
+        $this->book = $FB2Parser->getBook();
+    }
+
+    public function getBook()
+    {
+        return $this->book;
+    }
+
+    public function getAuthors()
+    {
+        return $this->book->authors;
+    }
+
+    public function getTranslators()
+    {
+        return $this->book->translators;
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function withImages(array $attributes): void
+    {
+        $this->insertAttributes(true, 'isImages');
+        foreach ($attributes as $key => $attribute) {
+            if ($key === 'directory') {
+                $this->insertAttributes($attribute, 'imagesDirectory');
+            }
+            if ($key === 'imagesWebPath') {
+                $this->insertAttributes((string)$attribute, 'imagesWebPath');
+            }
+        }
+        $fileSystem = new Filesystem();
+        try {
+            $fileSystem->mkdir($this->getAttributes()['imagesDirectory']);
+        } catch (IOExceptionInterface $exception) {
+            echo 'An error occurred while creating your directory at' . $exception->getPath();
+        }
+    }
+
+    public function withNotes(): void
+    {
+        $this->insertAttributes(true, 'isNotes');
+    }
 }
